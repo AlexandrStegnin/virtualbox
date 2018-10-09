3c73c2c2664759f4e4a5e709d3f189d0865e9819
AlexandrStegnin <alexandr.stegnin@gmail.com>
2018-10-09 16:23:02 +0500

    Добавлены методы CRUD для работы с ролями

--- src/main/java/ru/stegnin/virtualbox/controller/RoleController.java
+++ src/main/java/ru/stegnin/virtualbox/controller/RoleController.java
@@ -1,11 +1,10 @@
 package ru.stegnin.virtualbox.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.stegnin.virtualbox.model.Role;
 import ru.stegnin.virtualbox.repository.AuthRepository;
 import ru.stegnin.virtualbox.repository.RoleRepository;
@@ -13,6 +12,9 @@ import ru.stegnin.virtualbox.support.Constants;
 import ru.stegnin.virtualbox.support.GenericResponse;
 
 import javax.servlet.ServletContext;
+import javax.ws.rs.Produces;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 @RestController
@@ -43,6 +45,68 @@ public class RoleController {
         role = roleRepo.create(role);
         message = new GenericResponse.Builder().withMessage("Role : " + role + " created").build();
         return ResponseEntity.created(auth.getUri(this.getClass(), "create", (Object) role)).body(role);
+    }
+
+    /**
+     * Найти роль по id
+     * @param roleId - id пользователя
+     * @return - response с сообщением
+     */
+    @GetMapping(value = Constants.API_ROLES_ROLE_ID)
+    public ResponseEntity findById(@PathVariable(Constants.API_ROLE_ID) String roleId) {
+        Role role = roleRepo.findOne(roleId);
+
+        if (role == null) {
+            message = new GenericResponse.Builder().withMessage("Role with id : " + roleId + " not found").build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+        }
+        return ResponseEntity.ok(role);
+    }
+
+    /**
+     * Достать все роли
+     * @return - response со списком пользователей
+     */
+    @GetMapping
+    public ResponseEntity findAllRoles() {
+        List<Role> allRoles = new ArrayList<>(roleRepo.findAll());
+        return ResponseEntity.ok(allRoles);
+    }
+
+    /**
+     * Изменить роль
+     * @param roleId - id роли
+     * @param role - данные роли для изменения в формате json
+     * @return - response с сообщением
+     */
+    @PutMapping(value = Constants.API_ROLES_ROLE_ID)
+    @Produces(MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity update(@PathVariable(Constants.API_ROLE_ID) String roleId, @RequestBody Role role) {
+        role.setId(roleId);
+        role = roleRepo.update(role);
+        if (role != null) {
+            return ResponseEntity.ok().body(role);
+        } else {
+            message = new GenericResponse.Builder().withMessage("Role with id " + roleId + " not found.").build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+        }
+    }
+
+    /**
+     * Удалить роль по id
+     * @param roleId - id роли
+     * @return - response с сообщением
+     */
+    @DeleteMapping(value = Constants.API_ROLES_ROLE_ID)
+    public ResponseEntity remove(@PathVariable(Constants.API_ROLE_ID) String roleId) {
+        Role role = roleRepo.remove(roleId);
+        if (role != null) {
+            message = new GenericResponse.Builder().withMessage("Role with id " + roleId + " deleted successful.").build();
+            return ResponseEntity.ok().body(message);
+        } else {
+            message = new GenericResponse.Builder().withMessage("Role with id " + roleId + " not found.").build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+        }
     }
 
 }
