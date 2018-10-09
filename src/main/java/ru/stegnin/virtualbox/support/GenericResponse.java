eed915f366a02907941b92d4edf46bbb8c1867b0
AlexandrStegnin <alexandr.stegnin@gmail.com>
2018-10-09 16:48:07 +0500

    Добавлено поле для включения ошибок в тело ответа.

--- src/main/java/ru/stegnin/virtualbox/support/GenericResponse.java
+++ src/main/java/ru/stegnin/virtualbox/support/GenericResponse.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class GenericResponse {
     private String message;
+    private String error;
 
     public static class Builder {
         private GenericResponse newMessage;
@@ -15,6 +16,11 @@ public class GenericResponse {
 
         public Builder withMessage(String message) {
             newMessage.message = message;
+            return this;
+        }
+
+        public Builder withError(String error) {
+            newMessage.error = error;
             return this;
         }
 
