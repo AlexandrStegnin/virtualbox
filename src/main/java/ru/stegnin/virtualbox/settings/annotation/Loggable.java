06c03bae7a86964a822466981650650459ced484
AlexandrStegnin <alexandr.stegnin@gmail.com>
2018-10-26 15:40:54 +0500

    Добавлен SecurityUser, класс наследник UserDetails для удобной работы со SpringSecurity. Добавлено логирование методов. Добавлен в СКВ mvn-wrapper.jar

---
+++ src/main/java/ru/stegnin/virtualbox/settings/annotation/Loggable.java
@@ -0,0 +1,16 @@
+package ru.stegnin.virtualbox.settings.annotation;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import javax.interceptor.InterceptorBinding;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Qualifier
+@InterceptorBinding
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface Loggable {
+}
